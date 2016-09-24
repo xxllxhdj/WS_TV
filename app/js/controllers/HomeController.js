831415d975ee7f13525452a89f56f091d17cd57d
xxl <tx082xuxl@126.com>
2016-09-24 14:25:38 +0800

    add list

--- app/js/controllers/HomeController.js
+++ app/js/controllers/HomeController.js
@@ -1,24 +1,53 @@
 
 angular.module('WorkStationTV.controllers')
 
-.controller('HomeController', ['$scope', '$state', '$cordovaToast', function ($scope, $state, $cordovaToast) {
-	$scope.headerBtnLeft = function () {
-		if (window.cordova) {
-			$cordovaToast.showShortBottom('左侧按钮');
-		} else {
-			console.log('左侧按钮');
-		}
-	};
+.controller('HomeController', ['$scope', '$state', '$cordovaToast', 'TvDialog',
+	function ($scope, $state, $cordovaToast, TvDialog) {
+		$scope.headerBtnLeft = function () {
+			if (window.cordova) {
+				$cordovaToast.showShortBottom('左侧按钮');
+			} else {
+				console.log('左侧按钮');
+			}
+		};
 
-	$scope.goNext = function () {
-		$state.go('next');
-	};
+		$scope.goNext = function () {
+			$state.go('next');
+		};
 
-	$scope.onHold = function () {
-		if (window.cordova) {
-			$cordovaToast.showShortBottom('onHold');
-		} else {
-			console.log('onHold');
-		}
-	};
-}]);
+		$scope.onHold = function () {
+			if (window.cordova) {
+				$cordovaToast.showShortBottom('onHold');
+			} else {
+				console.log('onHold');
+			}
+		};
+
+		$scope.showList = function () {
+			TvDialog.showList({
+			    title: '列表',
+			    list: [
+			        { id: 1, name: '测试1' },
+			        { id: 2, name: '测试2' },
+			        { id: 3, name: '测试3' },
+			        { id: 4, name: '测试4' },
+			        { id: 5, name: '测试5' },
+			        { id: 6, name: '测试6' },
+			        { id: 7, name: '测试7' },
+			        { id: 8, name: '测试8' },
+			        { id: 9, name: '测试9' },
+			        { id: 10, name: '测试10'}
+			    ],
+			    valueField: 'id',
+			    displayField: 'name',
+			    value: 8
+			}).then(function (select) {
+			    if (window.cordova) {
+			    	$cordovaToast.showShortBottom(angular.toJson(select));
+			    } else {
+			    	console.log(angular.toJson(select));
+			    }
+			});
+		};
+	}
+]);
